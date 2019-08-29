d893fc6e347e58627f1a13dac500c680fb576260
Alfonso Venancio <alfonso.venancio22@gmail.com>
2019-08-29 09:16:03 -0500

    Primera actividad

--- Ejemplos/Clase 1/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Ejemplos/Clase 1/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.design.widget.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonHelloWorld = findViewById(R.id.buttonHelloWorld);
+        buttonHelloWorld.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Snackbar.make(findViewById(R.id.MainLayout), "Hola :)", Snackbar.LENGTH_SHORT);
+            }
+        });
     }
 
-    private void ShowMessage(){
-        
-    }
 }
